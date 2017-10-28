56c1677cfd244449bf72197d7b67940e0975c1b4
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 18:43:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     nil
   else
     lowest_key = name_hash.first[0]
-    lowest_value = name_has.first[1]
+    lowest_value = name_hash.first[1]
     name_hash.each do |key, value|
       if value < lowest_value
         lowest_value = value
