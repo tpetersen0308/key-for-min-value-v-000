02d24ae455021acaafaa318534613b58b3d055a1
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 18:43:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.empty? 
+  if name_hash.empty?
     nil
   else
     lowest_key = name_hash.first[0]
